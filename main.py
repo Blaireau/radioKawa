9320519f90a499e1f915da0cac86a7793aaf3a10
Blaireau <elblaireau85@gmail.com>
2020-02-18 10:47:47 +0100

    Working on getting on all infos of episodes in order to build a nice output (PDF ? Docx ? ODF ?)

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-page_to_parse = requests.get("https://www.radiokawa.com/episode/late-late-boudoir-gambetta-1/")
+#page_to_parse = requests.get("https://www.radiokawa.com/jeux-video/faster-than-light/")
+page_to_parse = requests.get("https://www.radiokawa.com/episode/late-late-boudoir-gambetta/")
 # page_to_parse = requests.get("https://www.radiokawa.com/culture-et-arts/comics-outcast/")
 # page_to_parse = requests.get("https://www.radiokawa.com/musique/morceaux-choisis/")
 parsed_page = BeautifulSoup(page_to_parse.text, features="html.parser")
 
-button_link = parsed_page.find_all("a", {"class": "download-button"})
-button_link = button_link[1:]
+# print(parsed_page)
 
+mp3_link_list = parsed_page.find_all("a", {"class": "download-button"})
+mp3_link_list = mp3_link_list[1:]
+
+for i in range(len(mp3_link_list)):
+    print(mp3_link_list[i]['href'])
+
+episode_link = parsed_page.find_all("a", {"class": "episode-link"})
+
+for i in range(len(episode_link)):
+    page_to_parse = requests.get(episode_link[i]['href'])
+    parsed_page = BeautifulSoup(page_to_parse.text, features="html.parser")
+    my_div = parsed_page.find_all("div", {"class": "episode-description"})
+    episode_title = parsed_page.find("h1", {"class": "episode-title"}).contents
+    episode_subtitle = parsed_page.find("div", {"class": "episode-subtitle"}).contents
+    episode_date = parsed_page.find("div", {"class": "episode-date"}).contents
+    episode_desc = parsed_page.find("div", {"class": "episode-content text-copy"}).contents
+    episode_thumbnail = str(parsed_page.find("div", {"class": "episode-thumbnail"})['style']).split('(')[1][:-2]
+    #print("Titre : "+str(episode_title[0])+"\nSous-titre : "+str(episode_subtitle[0])+"\nDate : "+str(episode_date)+"\nDescription : "+str(episode_desc)+"\n\n")
+    print(episode_thumbnail)
+
+
+'''
 for i in range(len(button_link)):
-    print(button_link[i]['href'])
+    print(button_link[i]) #['href'])
+'''
 
-episode_to_get = button_link[1]['href']
+'''
+episode_to_get = mp3_link_list[1]['href']
 episode = requests.get(episode_to_get)
 
-with open('./episode.mp3','wb') as f:
+with open('./episode.mp3', 'wb') as f:
     f.write(episode.content)
 
 print(episode.status_code)
 print(episode.headers['content-type'])
 print(episode.encoding)
+'''
+
